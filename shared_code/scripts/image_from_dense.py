a873180c404388941f44587d7652406c1ed20e95
AJStangl <ajstangl@gmail.com>
2023-06-17 22:09:26 -0400

    Good app

---
+++ shared_code/scripts/image_from_dense.py
@@ -0,0 +1,49 @@
+import io
+
+from shared_code.azure_storage.azure_file_system_adapter import AzureFileStorageAdapter
+from shared_code.azure_storage.tables import TableAdapter
+from PIL import Image
+
+if __name__ == '__main__':
+	from tqdm import tqdm
+
+	file_system = AzureFileStorageAdapter('data').get_file_storage()
+
+	client_dense_table = TableAdapter().get_table_client('denseCaptions')
+	with tqdm(total=len(list(client_dense_table.list_entities(select='id')))) as pbar:
+		for item in client_dense_table.list_entities():
+			try:
+				image_from_caption_path = f"data/image/dense/{item['id']}/{item['text'].replace(' ', '-')}.jpg"
+				image_path = f"data/image/{item['id']}.jpg"
+				if file_system.exists(image_from_caption_path):
+					item['image_from_caption_path'] = image_from_caption_path
+					client_dense_table.upsert_entity(item)
+					continue
+				else:
+					with file_system.open(image_path, 'rb') as f:
+						image = Image.open(f)
+						copied_image = image.copy()
+						image.close()
+
+						boundingBox_x = item['boundingBox_x']
+						boundingBox_y = item['boundingBox_y']
+						boundingBox_w = item['boundingBox_w']
+						boundingBox_h = item['boundingBox_h']
+
+						cropped_image = copied_image.crop((boundingBox_x, boundingBox_y, boundingBox_x + boundingBox_w, boundingBox_y + boundingBox_h))
+						resized_image = cropped_image.resize((512, int(512 * (cropped_image.height / cropped_image.width))))
+						copied_image.close()
+						cropped_image.close()
+						read_bytes_buffer = io.BytesIO()
+						resized_image.save(read_bytes_buffer, format='JPEG')
+						with file_system.open(image_from_caption_path, 'wb') as out:
+							out.write(read_bytes_buffer.getvalue())
+						resized_image.close()
+						item['image_from_caption_path'] = image_from_caption_path
+						client_dense_table.upsert_entity(item)
+			finally:
+				pbar.update(1)
+
+
+
+
--- shared_code/scripts/runner.py
+++ shared_code/scripts/runner.py
@@ -8,8 +8,8 @@ import json
 from shared_code.azure_storage.tables import TableAdapter
 
 
-def auto_azure_thumbnail(image_id: str, width: int = 512, height: int = 512, smartCropping: bool = True):
-	file_system = AzureFileStorageAdapter('data').get_file_storage()
+def auto_azure_thumbnail(_image_id: str, width: int = 512, height: int = 512, smartCropping: bool = True):
+	_file_system = AzureFileStorageAdapter('data').get_file_storage()
 	endpoint = f"vision/v3.1/generateThumbnail?width={width}&height={height}&smartCropping={smartCropping}"
 	base = os.environ["AZURE_VISION_ENDPOINT"]
 	url = f"{base}{endpoint}"
@@ -17,18 +17,16 @@ def auto_azure_thumbnail(image_id: str, width: int = 512, height: int = 512, sma
 		"Ocp-Apim-Subscription-Key": os.environ["AZURE_VISION_API_KEY"],
 	}
 	data = {
-		"url": file_system.url(f"data/image/{image_id}.jpg")
+		"url": _file_system.url(f"data/image/{_image_id}.jpg")
 	}
 	result = requests.post(url, headers=headers, data=json.dumps(data))
 
-	with file_system.open(f"data/image/azure/{image_id}.jpg", 'wb') as f:
+	with _file_system.open(f"data/image/azure/{_image_id}.jpg", 'wb') as f:
 		f.write(result.content)
 
-	return f"data/image/azure/{image_id}.jpg"
+	return f"data/image/azure/{_image_id}.jpg"
 
 
-def create_croppings_for_image():
-	pass
 
 if __name__ == '__main__':
 	from tqdm import tqdm
@@ -41,11 +39,10 @@ if __name__ == '__main__':
 	for entity in tqdm(entities, total=len(entities)):
 		image_id = entity['id']
 		if f"{image_id}.jpg" in extant:
-			print(f"Skipping {image_id}, already created...")
 			entity['azure_thumbnail_path'] = f"data/image/azure/{image_id}.jpg"
 			client.upsert_entity(entity)
 			continue
-		azure_thumbnail_path = auto_azure_thumbnail(image_id=entity['id'], smartCropping=False)
+		azure_thumbnail_path = auto_azure_thumbnail(_image_id=entity['id'], smartCropping=False)
 		if azure_thumbnail_path is not None:
 			entity['azure_thumbnail_path'] = azure_thumbnail_path
 		else:
